8b0510486524b848b2284f6f784fcfb48bd66642
exit <josephtaupa@switchmaven.com>
2021-03-16 16:39:41 +1000

    first commit

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
-// load the things we need
 var express = require('express');
 var app = express();
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
 
-// use res.render to load up an ejs view file
-
-// index page 
-app.get('/', function(req, res) {
-    var mascots = [
-        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-        { name: 'Tux', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-    ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
-
+    let data = [
+        {
+            name: "burt lannister",
+            age: 56,
+            hobbies: ['reading', 'polo']
+        },
+        {
+            name: "tobe nwige",
+            age: 27,
+            hobbies: ['writing', 'pressing flowers']
+        },
+        {
+            name: "miguel atwood",
+            age: undefined,
+            hobbies: ['music', 'mediation']
+        },
+        {
+            name: "hieu ngyen",
+            age: 33,
+            hobbies: undefined
+        },
+        {
+            name: undefined,
+            age: undefined,
+            hobbies: undefined
+        }
+    ]
     res.render('pages/index', {
-        mascots: mascots,
-        tagline: tagline
+        data: data
     });
 });
 
-// about page
-app.get('/about', function(req, res) {
-    res.render('pages/about');
-});
-
 app.listen(8080);
-console.log('8080 is the magic port');
+console.log('Listening at Port 8080');
